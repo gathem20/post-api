babe7b1a4426ddb8655de57e1f91cc0530f5d65c
gathem <anoneg655@gmail.com>
2024-04-04 01:16:03 +0200

    patch add it

--- index.js
+++ index.js
@@ -18,19 +18,42 @@ app.use(bodyParser.json());
 //     "https://ik.imagekit.io/higif0c3k/default-image.jpg?updatedAt=1712012215713",
 // });
 //start POST API
+
+const userss = [
+  {
+    id: 1,
+    name: " yousef",
+    description: "backend",
+    image: 123,
+  },
+  {
+    id: 2,
+    name: " yousef gathem",
+    description: "backend develpoer",
+    image: 1234,
+  },
+];
+app.get("/get", (req, res) => {
+  res.json(userss);
+});
 app.post("/post", async (req, res) => {
   try {
-    const { name, description, image } = req.body;
     // const uploadimage = await image_kit.upload({
     //   file: image,
     //   fileName: "",
     // });
-
-    const data = await database.insertOne({
+    const { name, description, image } = req.body;
+    const users = { name, description, image };
+    const id = Object.keys(users).length + 1;
+    users.id = id;
+    const data = await database.insertMany({
+      id,
       name,
       description,
       image,
     });
+
+    data.push(users);
     console.log(data);
     res.json({ name, description, image });
   } catch (err) {
@@ -40,29 +63,37 @@ app.post("/post", async (req, res) => {
 //Ending post api
 
 //start patch api
-
-app.patch("/patch/:id", async (req, res) => {
-  try {
-    const isValidObjectId = mongoose.isValidObjectId(req.params.id);
-    if (!isValidObjectId) {
-      return res.status(400).json({ message: "Invalid post ID" });
-    }
-
-    const postId = mongoose.Types.ObjectId(req.params.id);
-    const updates = req.body;
-    const updatedPost = await database.findOneAndUpdate(
-      { _id: postId },
-      { $set: updates },
-      { new: true }
-    );
-    if (!updatedPost)
-      return res.status(404).json({ message: "Failed to update the post" });
-    res.json(updatedPost);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to update the post" });
+app.patch("/patch/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, description, image } = req.body;
+  const user = userss.find((user) => user.id === parseInt(id));
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  } else {
+    user.name = name;
+    user.description = description;
+    user.image = image;
   }
+  res.json(user);
 });
+// app.patch("/patch/:id", async (req, res) => {
+//   try {
+
+//     const postId = database.Types.ObjectId(req.params.id);
+//     const updates = req.body;
+//     const updatedPost = await database.findOneAndUpdate(
+//       { _id: postId },
+//       { $set: updates },
+//       { new: true }
+//     );
+//     if (!updatedPost)
+//       return res.status(404).json({ message: "Failed to update the post" });
+//     res.json(updatedPost);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: "Failed to update the post2" });
+//   }
+// });
 //ending patch api
 const port = 3000;
 app.listen(port, (req, res) => {
